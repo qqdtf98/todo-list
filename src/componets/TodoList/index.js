9e968f13377ea98a1fbdd533f1e2fa2a6ee16481
Lee Sung Min <qqdtf98>
2020-04-29 17:59:17 +0900

    feat: create TodoList component

--- src/componets/TodoList/index.js
+++ src/componets/TodoList/index.js
@@ -1,20 +1,71 @@
-import React from 'react'
-import './index.css'
+import './index.scss'
 
-function SearchBar(){
-  return(
+import { Done, Todo } from '../CompoList'
+import React, { useState } from 'react'
+
+import search from '../../assets/images/search.svg'
+
+function SearchBar() {
+  return (
     <div className="search-wrapper">
-      <input className="searchinput" />
-      <div>icon</div>
+      <input placeholder="search" className="search-input" />
+      <img className="search-icon" src={search} alt="search" />
     </div>
   )
 }
 
-function TodoList(){
-  return(
+export const TodoContext = React.createContext([
+  {
+    title: '',
+    contents: '',
+    date: '',
+    state: false,
+    importance: '',
+  },
+])
+
+function TodoList() {
+  const [state, setState] = useState([
+    {
+      title: 'title',
+      contents: 'contents1',
+      date: '2020/04/29',
+      state: false,
+      importance: 'red',
+    },
+    {
+      title: 'title',
+      contents: 'contents2',
+      date: '2020/04/29',
+      state: false,
+      importance: 'red',
+    },
+    {
+      title: 'title',
+      contents: 'contents3',
+      date: '2020/04/29',
+      state: false,
+      importance: 'red',
+    },
+    {
+      title: 'title',
+      contents: 'contents4',
+      date: '2020/04/29',
+      state: false,
+      importance: 'red',
+    },
+  ])
+  // TODO push나 splice 할 때는 배열 복사해서 한 다음에 다시 setState하기
+  return (
     <div id="todo-list">
-      <h2>Todo List</h2>
+      <h1 className="todo-title">Todo List</h1>
       <SearchBar />
+      <div className="list-wrapper">
+        <TodoContext.Provider value={state}>
+          <Todo />
+          <Done />
+        </TodoContext.Provider>
+      </div>
     </div>
   )
 }
