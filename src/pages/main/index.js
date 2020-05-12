8f37b4a373e50711d327b95e6ffc1b1a92dbb860
qqdtf98 <qqdtf98@gmail.com>
2020-05-12 18:55:22 +0900

    chore: remove sample

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -12,25 +12,22 @@ function Main() {
     console.log(result)
   }
 
-  const handleClick = () => {
-    // api
-    //   .post('/user/get', {
-    //     data: {
-    //       id: 'lsm',
-    //     },
-    //   })
-    //   .then((res) => {
-    //     console.log(res.data)
-    //   })
-    Axios.post('http://localhost:80/user/get').then((res) => {
-      console.log(res)
-    })
-  }
+  // const handleClick = () => {
+  //   // api
+  //   //   .post('/user/get', {
+  //   //     data: {
+  //   //       id: 'lsm',
+  //   //     },
+  //   //   })
+  //   //   .then((res) => {
+  //   //     console.log(res)
+  //   //   })
+  // }
 
   return (
     <div id="main-page">
       <div className="main-title">Todo List</div>
-      <button onClick={handleClick}>button</button>
+      {/* <button onClick={handleClick}>button</button> */}
       {/* <GoogleLogin
         clientId="283233647825-oicrhle8givdtrv9ku1mt4ju9paeka56.apps.googleusercontent.com"
         render={(props) => (
